489cdb51a40ca7ebc4156c570480456139a64189
BAGOO\luozijian <luozijian@bagoo.com>
2023-11-13 13:49:03 +0800

    fix main.go bug

--- main.go
+++ main.go
@@ -1,2 +1,30 @@
 package main
 
+import (
+	"dingchatgpt/config"
+	"dingchatgpt/db"
+	"dingchatgpt/ding"
+	"flag"
+	"github.com/open-dingtalk/dingtalk-stream-sdk-go/logger"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	config.InitFlat()
+	flag.Parse()
+	//db.GptDBModel().Init()
+	db.Connect()
+
+	ding.StartDingTalk()
+
+	signChan := make(chan os.Signal, 1)
+
+	signal.Notify(signChan, syscall.SIGKILL, syscall.SIGTERM, os.Interrupt)
+
+	<-signChan
+
+	logger.GetLogger().Infof("app closed")
+}
+
